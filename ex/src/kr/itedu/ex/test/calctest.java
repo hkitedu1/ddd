6cd5b66cba6caf24237457597c9ddb7f7647d9dc
USER <USER@USER18-PC>
2018-09-21 10:44:56 +0900

    푸시푸시

---
+++ ex/src/kr/itedu/ex/test/calctest.java
@@ -0,0 +1,19 @@
+package kr.itedu.ex.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kr.itedu.ex.calc;
+
+class calctest {
+
+	@Test
+	void testSum() {
+		assertEquals(3,calc.sum(1, 2));
+		assertEquals(5,calc.sum(3, 2));
+		assertEquals(10,calc.sum(1, 9));
+	}
+
+}
